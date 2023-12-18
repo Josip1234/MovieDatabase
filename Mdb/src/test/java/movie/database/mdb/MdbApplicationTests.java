df0abceaf48c017f98f165b9f0dca91a7439d893
Josip Bošnjak <jbosnjak3@gmail.com>
2023-12-18 20:22:10 +0100

    imported new spring boot project

---
+++ Mdb/src/test/java/movie/database/mdb/MdbApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.database.mdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
